42e69f9e0e8cb854df5172da90487b0dff896bef
Andre Nascimento <andre4.20nascimento@outlook.com>
2023-04-18 21:23:42 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+double altura;
+string sexo;
+
+
+Console.WriteLine("\n--- Saiba o peso ideal ---");
+
+Console.WriteLine("Coloque \"M\" para o sexo: Mascolino");
+Console.WriteLine("Coloque \"F\" para o sexo: Feminino");
+sexo = Console.ReadLine()!.ToUpper();
+
+
+bool sexo1 = sexo == "F";
+bool sexo2 = sexo == "M";
+
+if (sexo1)
+{
+    Console.Write("\nColoque a sua Altura1...: ");
+    altura = Double.Parse(Console.ReadLine()!);
+
+    double pesoF = (altura * 62.1) - 44.7;
+    Console.WriteLine($"Seu peso ideal é {pesoF:N}Kg."); 
+}
+else if (sexo2)
+{
+    Console.Write("\nColoque a sua Altura2...: ");
+    altura = Double.Parse(Console.ReadLine()!);
+    double pesoF = (altura * 72.7) - 58.0;
+    Console.WriteLine($"Seu peso ideal é {pesoF:N}Kg.");
+}
+
+else 
+{
+    Console.WriteLine("Opção invalida");
+}
